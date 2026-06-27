ba5ca309870fc6b38711dfd58d004aa4db532021
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2026-06-27 09:53:10 +0300

    Setting up inventory in scene

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour
 {
     public bool slotIsFull;
-    public bool slotImage;
+    public Image slotImage;
     public ClothItemSO slotItem;
 
     public void SetImage(Sprite sprite) 
     { 
-        slotImage = sprite; 
+        slotImage.sprite = sprite; 
     }
 }
