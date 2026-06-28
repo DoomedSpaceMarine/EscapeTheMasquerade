59cdab703338d8369e532e0cb65c35008397ce45
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2026-06-28 12:37:53 +0300

    credits have been added to main menu

--- Assets/Scripts/UI/UI_TitleScreen.cs
+++ Assets/Scripts/UI/UI_TitleScreen.cs
@@ -5,17 +5,26 @@ using UnityEngine.SceneManagement;
 public class UI_TitleScreen : MonoBehaviour
 {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button creditsButton;
     [SerializeField] private Button quitButton;
+
+    [SerializeField] private GameObject creditsCanvas;
 
     private void Awake()
     {
         playButton.onClick.AddListener(() => PlayButton());
+        creditsButton.onClick.AddListener(() => CreditsButton());
         quitButton.onClick.AddListener(() => QuitButton());
     }
 
     private void PlayButton()
     {
         SceneManager.LoadScene("Introduction");
+    }
+
+    private void CreditsButton()
+    {
+        creditsCanvas.SetActive(true);
     }
 
     private void QuitButton()
