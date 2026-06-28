fe2f22081f3da69ac9a13e7f5f2aa0e138f46055
Teemu Nokkanen <teemu.nokkanen99@gmail.com>
2026-06-28 15:15:02 +0300

    Inventory slot fix attempt

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -6,6 +6,15 @@ public class InventorySlot : MonoBehaviour
     public bool slotIsFull;
     public Image slotImage;
     public ClothItemSO slotItem;
+    public Sprite emptySprite;
+
+    private void Update()
+    {
+        if (!slotIsFull)
+        {
+            slotImage.sprite = emptySprite;
+        }
+    }
 
     public void SetImage(Sprite sprite) 
     { 
